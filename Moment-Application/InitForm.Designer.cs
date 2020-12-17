4b99543432aa3daa0fb51cfcfb3902ef4f172b17
Marius-Dom <weihanjun2007@163.com>
2020-12-18 00:50:21 +0800

    Basically done the UI design of the InitForm

--- Moment-Application/InitForm.Designer.cs
+++ Moment-Application/InitForm.Designer.cs
@@ -30,62 +30,277 @@ namespace Moment_Application
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InitForm));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.iconBox = new System.Windows.Forms.PictureBox();
+            this.mainLabel1 = new System.Windows.Forms.Label();
+            this.backbroadBox = new System.Windows.Forms.GroupBox();
+            this.mainLabel2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.colorSelectButton1 = new System.Windows.Forms.Button();
+            this.colorSelectButton2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.iconBox)).BeginInit();
+            this.backbroadBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // iconBox
             // 
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 39);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.iconBox.BackColor = System.Drawing.Color.Transparent;
+            this.iconBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("iconBox.BackgroundImage")));
+            this.iconBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.iconBox.Location = new System.Drawing.Point(12, 12);
+            this.iconBox.Name = "iconBox";
+            this.iconBox.Size = new System.Drawing.Size(64, 64);
+            this.iconBox.TabIndex = 0;
+            this.iconBox.TabStop = false;
+            // 
+            // mainLabel1
+            // 
+            this.mainLabel1.AutoSize = true;
+            this.mainLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.mainLabel1.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.mainLabel1.Location = new System.Drawing.Point(82, 12);
+            this.mainLabel1.Name = "mainLabel1";
+            this.mainLabel1.Size = new System.Drawing.Size(312, 27);
+            this.mainLabel1.TabIndex = 1;
+            this.mainLabel1.Text = "欢迎使用\"此刻\"学习时间管理软件";
+            // 
+            // backbroadBox
+            // 
+            this.backbroadBox.BackColor = System.Drawing.Color.White;
+            this.backbroadBox.Controls.Add(this.mainLabel2);
+            this.backbroadBox.Controls.Add(this.mainLabel1);
+            this.backbroadBox.Controls.Add(this.iconBox);
+            this.backbroadBox.Location = new System.Drawing.Point(0, 0);
+            this.backbroadBox.Name = "backbroadBox";
+            this.backbroadBox.Size = new System.Drawing.Size(412, 93);
+            this.backbroadBox.TabIndex = 2;
+            this.backbroadBox.TabStop = false;
+            // 
+            // mainLabel2
+            // 
+            this.mainLabel2.AutoSize = true;
+            this.mainLabel2.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.mainLabel2.Location = new System.Drawing.Point(84, 49);
+            this.mainLabel2.Name = "mainLabel2";
+            this.mainLabel2.Size = new System.Drawing.Size(273, 14);
+            this.mainLabel2.TabIndex = 2;
+            this.mainLabel2.Text = "在我们开始之前，请先完成一些初始化设置";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(82, 39);
+            this.label1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(41, 109);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(312, 27);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "欢迎使用\"此刻\"学习时间管理软件";
+            this.label1.Size = new System.Drawing.Size(35, 14);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "昵称";
             // 
-            // button1
+            // textBox1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.Control;
-            this.button1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button1.Location = new System.Drawing.Point(369, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(31, 32);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "✕";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.textBox1.Location = new System.Drawing.Point(87, 106);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(270, 21);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(41, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 14);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "我上一次考试的总分数";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(194, 138);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(163, 21);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(180, 200);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(177, 21);
+            this.numericUpDown2.TabIndex = 8;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(41, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 14);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "下一次考试想要提高";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(41, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(203, 14);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "目标每天记录学习时间（分钟）";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(240, 232);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(117, 21);
+            this.numericUpDown3.TabIndex = 10;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(194, 169);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(163, 21);
+            this.numericUpDown4.TabIndex = 12;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            700,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(41, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(147, 14);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "我上一次考试的满分是";
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.confirmButton.Location = new System.Drawing.Point(143, 266);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(91, 35);
+            this.confirmButton.TabIndex = 13;
+            this.confirmButton.Text = "初始化";
+            this.confirmButton.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(275, 309);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 14);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "颜色风格";
+            // 
+            // colorSelectButton1
+            // 
+            this.colorSelectButton1.BackColor = System.Drawing.Color.White;
+            this.colorSelectButton1.Location = new System.Drawing.Point(344, 304);
+            this.colorSelectButton1.Name = "colorSelectButton1";
+            this.colorSelectButton1.Size = new System.Drawing.Size(25, 25);
+            this.colorSelectButton1.TabIndex = 15;
+            this.colorSelectButton1.UseVisualStyleBackColor = false;
+            this.colorSelectButton1.Click += new System.EventHandler(this.colorSelectButton1_Click);
+            // 
+            // colorSelectButton2
+            // 
+            this.colorSelectButton2.BackColor = System.Drawing.Color.Black;
+            this.colorSelectButton2.Location = new System.Drawing.Point(375, 304);
+            this.colorSelectButton2.Name = "colorSelectButton2";
+            this.colorSelectButton2.Size = new System.Drawing.Size(25, 25);
+            this.colorSelectButton2.TabIndex = 16;
+            this.colorSelectButton2.UseVisualStyleBackColor = false;
+            this.colorSelectButton2.Click += new System.EventHandler(this.colorSelectButton2_Click);
             // 
             // InitForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(412, 471);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(412, 338);
+            this.Controls.Add(this.colorSelectButton2);
+            this.Controls.Add(this.colorSelectButton1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.confirmButton);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Controls.Add(this.backbroadBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "InitForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "\"此刻\"学习时间管理软件";
-            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.InitForm_MouseDown);
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.InitForm_MouseMove);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.InitForm_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.InitForm_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.iconBox)).EndInit();
+            this.backbroadBox.ResumeLayout(false);
+            this.backbroadBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,8 +308,23 @@ namespace Moment_Application
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox iconBox;
+        private System.Windows.Forms.Label mainLabel1;
+        private System.Windows.Forms.GroupBox backbroadBox;
+        private System.Windows.Forms.Label mainLabel2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button confirmButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button colorSelectButton1;
+        private System.Windows.Forms.Button colorSelectButton2;
     }
 }
--- Moment-Application/InitForm.cs
+++ Moment-Application/InitForm.cs
@@ -17,29 +17,78 @@ namespace Moment_Application
             InitializeComponent();
         }
 
-        private Point mousePoint = new Point();
-
-        private void InitForm_MouseDown(object sender, MouseEventArgs e)
+        private void InitForm_FormClosing(object sender, FormClosingEventArgs e)
         {
-            mousePoint.X = e.X;
-            mousePoint.Y = e.Y;
+            DialogResult result = MessageBox.Show(this, "软件还未完成初始化设置, 您确定要退出吗?", "退出", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+                e.Cancel = true;
         }
 
-        private void InitForm_MouseMove(object sender, MouseEventArgs e)
+        private void InitForm_FormClosed(object sender, FormClosedEventArgs e)
         {
-            if (e.Button == MouseButtons.Left)
+            if (e.CloseReason == CloseReason.UserClosing)
+                Application.Exit();
+        }
+
+        private void colorSelectButton2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(this, "您将切换至 深色模式 , 确认?", "主题切换", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
             {
-                Point Posittion = MousePosition;
-                Posittion.Offset(-mousePoint.X, -mousePoint.Y);
-                Location = Posittion;
+                Color myGray = Color.FromArgb(50, 50, 50);
+                Color myBlack = Color.FromArgb(30, 30, 30);
+                this.BackColor = myBlack;
+                backbroadBox.BackColor = myGray;
+                mainLabel1.ForeColor = Color.White;
+                mainLabel2.ForeColor = Color.White;
+                label1.ForeColor = Color.White;
+                label2.ForeColor = Color.White;
+                label3.ForeColor = Color.White;
+                label4.ForeColor = Color.White;
+                label5.ForeColor = Color.White;
+                label6.ForeColor = Color.White;
+                textBox1.BackColor = myGray;
+                textBox1.ForeColor = Color.White;
+                numericUpDown1.BackColor = myGray;
+                numericUpDown1.ForeColor = Color.White;
+                numericUpDown2.BackColor = myGray;
+                numericUpDown2.ForeColor = Color.White;
+                numericUpDown3.BackColor = myGray;
+                numericUpDown3.ForeColor = Color.White;
+                numericUpDown4.BackColor = myGray;
+                numericUpDown4.ForeColor = Color.White;
+                confirmButton.BackColor = myGray;
+                confirmButton.ForeColor = Color.White;
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void colorSelectButton1_Click(object sender, EventArgs e)
         {
-            if (MessageBox.Show(this, "软件还未完成初始化设置, 您确定要退出吗?", "退出", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            DialogResult result = MessageBox.Show(this, "您将切换至 浅色模式 , 确认?", "主题切换", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
             {
-                Application.Exit(); 
+                this.BackColor = Control.DefaultBackColor;
+                backbroadBox.BackColor = Color.White;
+                mainLabel1.ForeColor = Color.Black;
+                mainLabel2.ForeColor = Color.Black;
+                label1.ForeColor = Color.Black;
+                label2.ForeColor = Color.Black;
+                label3.ForeColor = Color.Black;
+                label4.ForeColor = Color.Black;
+                label5.ForeColor = Color.Black;
+                label6.ForeColor = Color.Black;
+                textBox1.BackColor = Color.White;
+                textBox1.ForeColor = Color.Black;
+                numericUpDown1.BackColor = Color.White;
+                numericUpDown1.ForeColor = Color.Black;
+                numericUpDown2.BackColor = Color.White;
+                numericUpDown2.ForeColor = Color.Black;
+                numericUpDown3.BackColor = Color.White;
+                numericUpDown3.ForeColor = Color.Black;
+                numericUpDown4.BackColor = Color.White;
+                numericUpDown4.ForeColor = Color.Black;
+                confirmButton.BackColor = Control.DefaultBackColor;
+                confirmButton.ForeColor = Color.Black;
             }
         }
     }
