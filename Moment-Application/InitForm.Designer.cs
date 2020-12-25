8e4ecb47367669717a53d6137acfc9282e007361
Marius-Dom <weihanjun2007@163.com>
2020-12-25 22:04:13 +0800

    Change components.

--- Moment-Application/InitForm.Designer.cs
+++ Moment-Application/InitForm.Designer.cs
@@ -37,23 +37,19 @@ namespace Moment_Application
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
-            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
             this.label5 = new System.Windows.Forms.Label();
             this.confirmButton = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.colorSelectButton1 = new System.Windows.Forms.Button();
             this.colorSelectButton2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.iconBox)).BeginInit();
             this.backbroadBox.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
             this.SuspendLayout();
             // 
             // iconBox
@@ -104,7 +100,7 @@ namespace Moment_Application
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(41, 109);
+            this.label1.Location = new System.Drawing.Point(51, 109);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 14);
             this.label1.TabIndex = 3;
@@ -112,7 +108,7 @@ namespace Moment_Application
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(87, 106);
+            this.textBox1.Location = new System.Drawing.Point(97, 106);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(270, 21);
             this.textBox1.TabIndex = 4;
@@ -121,56 +117,17 @@ namespace Moment_Application
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(41, 141);
+            this.label2.Location = new System.Drawing.Point(51, 136);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(147, 14);
             this.label2.TabIndex = 5;
             this.label2.Text = "我上一次考试的总分数";
             // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(194, 138);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
-            1000,
-            0,
-            0,
-            0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(163, 21);
-            this.numericUpDown1.TabIndex = 6;
-            this.numericUpDown1.Value = new decimal(new int[] {
-            600,
-            0,
-            0,
-            0});
-            // 
-            // numericUpDown2
-            // 
-            this.numericUpDown2.Location = new System.Drawing.Point(180, 200);
-            this.numericUpDown2.Maximum = new decimal(new int[] {
-            1000,
-            0,
-            0,
-            0});
-            this.numericUpDown2.Minimum = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(177, 21);
-            this.numericUpDown2.TabIndex = 8;
-            this.numericUpDown2.Value = new decimal(new int[] {
-            20,
-            0,
-            0,
-            0});
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label3.Location = new System.Drawing.Point(41, 203);
+            this.label3.Location = new System.Drawing.Point(51, 190);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(133, 14);
             this.label3.TabIndex = 7;
@@ -180,61 +137,17 @@ namespace Moment_Application
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label4.Location = new System.Drawing.Point(41, 235);
+            this.label4.Location = new System.Drawing.Point(51, 217);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(203, 14);
             this.label4.TabIndex = 9;
             this.label4.Text = "目标每天记录学习时间（分钟）";
             // 
-            // numericUpDown3
-            // 
-            this.numericUpDown3.Location = new System.Drawing.Point(240, 232);
-            this.numericUpDown3.Maximum = new decimal(new int[] {
-            600,
-            0,
-            0,
-            0});
-            this.numericUpDown3.Minimum = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            this.numericUpDown3.Name = "numericUpDown3";
-            this.numericUpDown3.Size = new System.Drawing.Size(117, 21);
-            this.numericUpDown3.TabIndex = 10;
-            this.numericUpDown3.Value = new decimal(new int[] {
-            120,
-            0,
-            0,
-            0});
-            // 
-            // numericUpDown4
-            // 
-            this.numericUpDown4.Location = new System.Drawing.Point(138, 169);
-            this.numericUpDown4.Maximum = new decimal(new int[] {
-            1000,
-            0,
-            0,
-            0});
-            this.numericUpDown4.Minimum = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            this.numericUpDown4.Name = "numericUpDown4";
-            this.numericUpDown4.Size = new System.Drawing.Size(219, 21);
-            this.numericUpDown4.TabIndex = 12;
-            this.numericUpDown4.Value = new decimal(new int[] {
-            700,
-            0,
-            0,
-            0});
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label5.Location = new System.Drawing.Point(41, 172);
+            this.label5.Location = new System.Drawing.Point(51, 163);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(91, 14);
             this.label5.TabIndex = 11;
@@ -243,7 +156,7 @@ namespace Moment_Application
             // confirmButton
             // 
             this.confirmButton.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.confirmButton.Location = new System.Drawing.Point(143, 266);
+            this.confirmButton.Location = new System.Drawing.Point(148, 250);
             this.confirmButton.Name = "confirmButton";
             this.confirmButton.Size = new System.Drawing.Size(91, 35);
             this.confirmButton.TabIndex = 13;
@@ -255,7 +168,7 @@ namespace Moment_Application
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label6.Location = new System.Drawing.Point(275, 309);
+            this.label6.Location = new System.Drawing.Point(275, 291);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(63, 14);
             this.label6.TabIndex = 14;
@@ -264,7 +177,7 @@ namespace Moment_Application
             // colorSelectButton1
             // 
             this.colorSelectButton1.BackColor = System.Drawing.Color.White;
-            this.colorSelectButton1.Location = new System.Drawing.Point(344, 304);
+            this.colorSelectButton1.Location = new System.Drawing.Point(344, 286);
             this.colorSelectButton1.Name = "colorSelectButton1";
             this.colorSelectButton1.Size = new System.Drawing.Size(25, 25);
             this.colorSelectButton1.TabIndex = 15;
@@ -274,30 +187,58 @@ namespace Moment_Application
             // colorSelectButton2
             // 
             this.colorSelectButton2.BackColor = System.Drawing.Color.Black;
-            this.colorSelectButton2.Location = new System.Drawing.Point(375, 304);
+            this.colorSelectButton2.Location = new System.Drawing.Point(375, 286);
             this.colorSelectButton2.Name = "colorSelectButton2";
             this.colorSelectButton2.Size = new System.Drawing.Size(25, 25);
             this.colorSelectButton2.TabIndex = 16;
             this.colorSelectButton2.UseVisualStyleBackColor = false;
             this.colorSelectButton2.Click += new System.EventHandler(this.colorSelectButton2_Click);
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(204, 133);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(163, 21);
+            this.textBox2.TabIndex = 17;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(148, 160);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(219, 21);
+            this.textBox3.TabIndex = 18;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(190, 187);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(177, 21);
+            this.textBox4.TabIndex = 19;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(260, 214);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(107, 21);
+            this.textBox5.TabIndex = 20;
+            // 
             // InitForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(412, 338);
+            this.ClientSize = new System.Drawing.Size(412, 321);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.colorSelectButton2);
             this.Controls.Add(this.colorSelectButton1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.confirmButton);
-            this.Controls.Add(this.numericUpDown4);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.numericUpDown3);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.numericUpDown2);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
@@ -313,10 +254,6 @@ namespace Moment_Application
             ((System.ComponentModel.ISupportInitialize)(this.iconBox)).EndInit();
             this.backbroadBox.ResumeLayout(false);
             this.backbroadBox.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -331,16 +268,16 @@ namespace Moment_Application
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.NumericUpDown numericUpDown2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.NumericUpDown numericUpDown3;
-        private System.Windows.Forms.NumericUpDown numericUpDown4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button confirmButton;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button colorSelectButton1;
         private System.Windows.Forms.Button colorSelectButton2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
